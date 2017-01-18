f889f94e46177ad414404ad13c61f5740662644a
gness1804 <graham.nessler@gmail.com>
2017-01-18 11:54:07 -0700

    Delete commented code

--- app/bookmark.service.ts
+++ app/bookmark.service.ts
@@ -10,19 +10,3 @@ export class BookmarkService {
   };
 
 }
-
-
-// import { Injectable } from '@angular/core';
-//
-// import { Hero } from './hero';
-// import { HEROES } from './mock-heroes';
-//
-// @Injectable()
-// export class HeroService {
-//   getHeroes(): Promise<Hero[]> {
-//     return Promise.resolve(HEROES);
-//   };
-//   getHero(id: number): Promise<Hero> {
-//   return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
-//   };
-// }
