1e48ef6256c489d9c0f4b9ee4a860cad2bd82cda
gness1804 <graham.nessler@gmail.com>
2017-01-18 16:24:03 -0700

    Format nav bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: [ 'app.component.css' ],
   template: `
   <h1>{{title}}</h1>
-  <a routerLink="/bookmarks" routerLinkActive="active" >Bookmarks</a>
-  <a routerLink="/questions" routerLinkActive="active" >Questions</a>
+  <nav>
+    <a routerLink="/bookmarks" routerLinkActive="active">Bookmarks</a>
+    <a routerLink="/questions" routerLinkActive="active">Questions</a>
+  </nav>
   <router-outlet></router-outlet>
   `,
 })
--- app/questions-data.ts
+++ app/questions-data.ts
@@ -4,14 +4,14 @@ export const QUESTIONS: Question[] = [
   {
     id: 1,
     body: 'How can you avoid callback hells?',
-    answer: 'modularization, control flow libraries, generators with promises, async/await',
+    answer: '"Modularization, control flow libraries, generators with promises, async/await"',
     sourceName: 'Node.js Interview Questions and Answers ',
     sourceUrl: 'https://blog.risingstack.com/node-js-interview-questions-and-answers-2017/',
   },
   {
     id: 2,
     body: 'What happens when you call setState?',
-    answer: 'The first thing React will do when setState is called is merge the object you passed into setState into the current state of the component. This will kick off a process called reconciliation. The end goal of reconciliation is to, in the most efficient way possible, update the UI based on this new state. To do this, React will construct a new tree of React elements (which you can think of as an object representation of your UI). Once it has this tree, in order to figure out how the UI should change in response to the new state, React will diff this new tree against the previous element tree. By doing this, React will then know the exact changes which occurred, and by knowing exactly what changes occurred, will able to minimize its footprint on the UI by only making updates where absolutely necessary.',
+    answer: '"The first thing React will do when setState is called is merge the object you passed into setState into the current state of the component. This will kick off a process called reconciliation. The end goal of reconciliation is to, in the most efficient way possible, update the UI based on this new state. To do this, React will construct a new tree of React elements (which you can think of as an object representation of your UI). Once it has this tree, in order to figure out how the UI should change in response to the new state, React will diff this new tree against the previous element tree. By doing this, React will then know the exact changes which occurred, and by knowing exactly what changes occurred, will able to minimize its footprint on the UI by only making updates where absolutely necessary."',
     sourceName: 'React Interview Questions',
     sourceUrl: 'https://tylermcginnis.com/react-interview-questions/',
   }
